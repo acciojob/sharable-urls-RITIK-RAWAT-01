d3902f9a426b30d569da4bd28034434677212053
RITIK-RAWAT-01 <ritvikrawat35100@gmail.com>
2025-04-09 17:16:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // your code here
+let Name = document.getElementById('name')
+let year = document.getElementById('year')
+let button = document.getElementById('button')
+console.log(button)
+let url = document.getElementById('url')
+
+
+button.addEventListener("click" , () => {
+
+    if(!(Name.value == "" || year.value == "")){
+        url.append(`?name=${Name.value}&year=${year.value}`)
+    }
+    else if(Name.value != "" || year.value == ""){
+        url.append(`?name=${Name.value}`);
+    }
+    else if(Name.value == "" || year.value != ""){
+        url.append(`?year=${year.value}`)
+    }
+})
